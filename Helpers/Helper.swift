d73eef9c0c1185676f04b951c1f3a173bae956ba
jajimentol <aniljturan@gmail.com>
2021-05-27 12:46:26 +0300

    test items added

---
+++ Helpers/Helper.swift
@@ -0,0 +1,18 @@
+//
+//  Helper.swift
+//  rmcase
+//
+//  Created by ANIL Turan on 27.05.2021.
+//
+
+import Foundation
+import UIKit
+
+public var screenWidth: CGFloat {
+    return UIScreen.main.bounds.width
+}
+
+// Screen height.
+public var screenHeight: CGFloat {
+    return UIScreen.main.bounds.height
+}
