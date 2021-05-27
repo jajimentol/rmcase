ea52eb4c78999d38657ae583f39cf0a130ccfd0b
jajimentol <aniljturan@gmail.com>
2021-05-27 12:46:52 +0300

    collectionview setup for test

--- Scenes/Characters/CharactersViewController.swift
+++ Scenes/Characters/CharactersViewController.swift
@@ -14,7 +14,8 @@ final class CharactersViewController: BaseViewController {
     
     let titleLabel = UILabel()
     
-    let collectionView = UICollectionView()
+    let flowLayout = UICollectionViewFlowLayout()
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,28 +23,63 @@ final class CharactersViewController: BaseViewController {
         setInterface()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+    
     func setInterface() {
         
         view.backgroundColor = UIColor(hex: "131415")
         
         titleLabel.text = "Characters"
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 18.0)
-        titleLabel.backgroundColor = .white
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 24.0)
+        titleLabel.textColor = .white
         view.addSubview(titleLabel)
         titleLabel.snp.makeConstraints { (make) in
             make.left.equalTo(view).offset(20)
-            make.top.equalTo(view).offset(65)
+            make.top.equalTo(view).offset(110)
         }
         
-        collectionView.register(CharacterCollectionViewCell.self, forCellWithReuseIdentifier: "collectionCell")
-        collectionView.backgroundColor = .clear
-        view.addSubview(collectionView)
-        collectionView.snp.makeConstraints { (make) in
-            make.left.equalTo(titleLabel)
-            make.top.equalTo(titleLabel.snp.bottom).offset(20)
-        }
-        
-        
+        setupCollectionView()
         
     }
+    
+    func setupCollectionView() {
+        let itemWidth = (screenWidth - 60) / 2
+        let itemHeight = itemWidth * 1.34
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        flowLayout.minimumInteritemSpacing = 20
+        flowLayout.minimumLineSpacing = 30
+        
+        collectionView.setCollectionViewLayout(flowLayout, animated: false)
+        collectionView.register(CharacterCollectionViewCell.self, forCellWithReuseIdentifier: "collectionCell")
+        collectionView.backgroundColor = .clear
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+        collectionView.snp.makeConstraints { (make) in
+            make.left.equalTo(view).offset(20)
+            make.right.equalTo(view).offset(-20)
+            make.top.equalTo(titleLabel.snp.bottom).offset(20)
+            make.bottom.equalTo(view)
+        }
+        collectionView.reloadData()
+    }
+}
+
+extension CharactersViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as? CharacterCollectionViewCell else { return UICollectionViewCell() }
+        cell.fillCell()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+           return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        }
+    
 }
